029c03efc4b0b41890df6e3d0a61c22b8e213467
hayashidareo <reo0524h@gmail.com>
2025-10-27 15:06:19 +0900

    first commit

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// .env.localから接続情報を読み込む
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+// Next.jsの高速リロード（HMR）に対応するため、すでに初期化済みかチェックする
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+// 各サービスをエクスポートする
+export const db = getFirestore(app);
+export const auth = getAuth(app);
