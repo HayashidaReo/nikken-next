db68a5ba55f085b1fc80e0d740e85c61cdeb6f9f
hayashidareo <reo0524h@gmail.com>
2025-11-14 12:56:58 +0900

    ドラッグ中にも差分検出可能に

--- src/components/organisms/match-setup-table.tsx
+++ src/components/organisms/match-setup-table.tsx
@@ -340,6 +340,7 @@ export function MatchSetupTable({
                       round: Boolean(rowChanges.round),
                       playerA: Boolean(rowChanges.playerA),
                       playerB: Boolean(rowChanges.playerB),
+                      sortOrder: Boolean(rowChanges.sortOrder),
                     }}
                     isAdded={false}
                     isDeleted={false}
