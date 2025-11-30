2dfa38ddacd23317b424755e7429bb6ea4312987
HayashidaReo <hayashida.dev@gmail.com>
2025-11-30 22:45:28 +0900

    選手名などのフォント調節コンポーネント作成

---
+++ src/components/atoms/adjust-horizontal-text.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React from "react";
+import { useResponsiveFont } from "@/hooks/useResponsiveFont";
+import { cn } from "@/lib/utils/utils";
+
+interface AdjustHorizontalTextProps extends React.HTMLAttributes<HTMLDivElement> {
+    baseFontSize: number;
+    minFontSize: number;
+    maxWidth: number;
+    textContent: string;
+}
+
+export function AdjustHorizontalText({
+    baseFontSize,
+    minFontSize,
+    maxWidth,
+    textContent,
+    className,
+    style,
+    ...props
+}: AdjustHorizontalTextProps) {
+    const { fontSizeRem, elementRef } = useResponsiveFont({
+        baseFontSize,
+        minFontSize,
+        maxWidth,
+        textContent,
+    });
+
+    return (
+        <div
+            ref={elementRef}
+            style={{
+                fontSize: `${fontSizeRem}rem`,
+                maxWidth: `${maxWidth}px`,
+                ...style,
+            }}
+            className={cn("whitespace-nowrap", className)}
+            {...props}
+        >
+            {textContent}
+        </div>
+    );
+}
