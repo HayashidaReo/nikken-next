3c2687e6b3cce2fd4c5be2ce9440532f5d2ed4ee
HayashidaReo <hayashida.dev@gmail.com>
2025-12-02 02:31:32 +0900

    不要コード削除

--- src/services/sync/match-group-sync.ts
+++ src/services/sync/match-group-sync.ts
@@ -1,11 +1,8 @@
 import { FirestoreMatchGroupRepository } from '@/repositories/firestore/match-group-repository';
-import { FirestoreTeamMatchRepository } from '@/repositories/firestore/team-match-repository';
 import { localMatchGroupRepository } from '@/repositories/local/match-group-repository';
-import { localTeamMatchRepository } from '@/repositories/local/team-match-repository';
 import { syncItems } from './utils';
 
 const matchGroupRepository = new FirestoreMatchGroupRepository();
-const teamMatchRepository = new FirestoreTeamMatchRepository();
 
 export async function uploadMatchGroups(orgId: string, tournamentId: string): Promise<number> {
     const unsyncedMatchGroups = await localMatchGroupRepository.getUnsynced(orgId, tournamentId);
@@ -41,39 +38,4 @@ export async function uploadMatchGroups(orgId: string, tournamentId: string): Pr
     );
 }
 
-export async function uploadTeamMatches(orgId: string, tournamentId: string): Promise<number> {
-    const unsyncedTeamMatches = await localTeamMatchRepository.getUnsynced(orgId, tournamentId);
 
-    if (unsyncedTeamMatches.length === 0) {
-        return 0;
-    }
-
-    return await syncItems(
-        unsyncedTeamMatches,
-        "team match",
-        (m) => m.matchId,
-        async (teamMatch, id) => {
-            if (!teamMatch.matchGroupId) throw new Error("Missing matchGroupId");
-            await teamMatchRepository.update(orgId, tournamentId, teamMatch.matchGroupId, id, {
-                matchId: id,
-                matchGroupId: teamMatch.matchGroupId,
-                roundId: teamMatch.roundId,
-                players: teamMatch.players,
-                sortOrder: teamMatch.sortOrder,
-                isCompleted: teamMatch.isCompleted,
-                winner: teamMatch.winner,
-                winReason: teamMatch.winReason,
-            });
-        },
-        async (teamMatch, id) => {
-            if (!teamMatch.matchGroupId) throw new Error("Missing matchGroupId");
-            await teamMatchRepository.delete(orgId, tournamentId, teamMatch.matchGroupId, id);
-        },
-        async (teamMatch) => {
-            if (teamMatch.matchId) await localTeamMatchRepository.markAsSynced(teamMatch.matchId);
-        },
-        async (id) => {
-            await localTeamMatchRepository.hardDelete(id);
-        }
-    );
-}
