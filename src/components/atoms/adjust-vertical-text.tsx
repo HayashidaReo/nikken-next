c88d27f4e55d7d4b91f9126e73186888b3358a1e
HayashidaReo <hayashida.dev@gmail.com>
2025-12-03 15:29:18 +0900

    選手名を左に移動する

--- src/components/atoms/adjust-vertical-text.tsx
+++ src/components/atoms/adjust-vertical-text.tsx
@@ -35,10 +35,11 @@ export function AdjustVerticalText({
                 writingMode: "vertical-rl",
                 textOrientation: "upright",
                 whiteSpace: "nowrap",
-                lineHeight: "1.2",
+                lineHeight: "1",
                 fontSize: `${fontSizeRem}rem`,
                 maxHeight: `${maxHeight}px`,
                 overflow: "hidden",
+                transform: "translateX(-3%)", // 微調整: 少し左に寄せる
                 ...style,
             }}
             className={cn("", className)}
